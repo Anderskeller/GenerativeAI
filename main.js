3b697df11af80b25a46c15641b894f8deb494a8d
Anderskeller <anderskeller00@gmail.com>
2023-11-27 23:46:27 +0100

    Endelige opgave

--- main.js
+++ main.js
@@ -1,39 +1,57 @@
-const sendButton = document.getElementById("submitButton");
-const resetButton = document.getElementById("reset");
-const input = document.getElementById('story'); // Updated to match the input ID in your HTML
-const spanGeneratedTravel = document.getElementById("travel-result");
-const loadingSpan = document.getElementById("hidden-loading");
 
-sendButton.addEventListener("click", function () {
-    const travelDescriptionFromUser = input.value;
-    loadingSpan.classList.remove('hidden');
+    const sendButton = document.getElementById("submitButton");
+    const resetButton = document.getElementById("reset");
+    const input = document.getElementById('generate');
+    const spanGeneratedTravel = document.getElementById("travel-result");
+    const loadingSpan = document.getElementById("hidden-loading");
 
-    fetch("http://localhost:11434/api/generate", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            model: "llama2",
-            prompt: input.value,
-            stream: false
+
+    sendButton.addEventListener("click", function () {
+        const travelPlanerDescription = input.value;
+        loadingSpan.classList.remove('hidden');
+
+
+        fetch("http://localhost:11434/api/generate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                model: "llama2",
+                prompt: travelPlanerDescription,
+                stream: false
+            })
         })
-    })
-        .then(response => response.json())
-        .then(responseText => {
-            // Update the displayed result with the response from the chatbot
-            spanGeneratedTravel.textContent = responseText.generated_text;
-            loadingSpan.classList.add('hidden'); // Hide the loading indicator
-        })
-        .catch(error => {
-            console.error("Error:", error);
-            // Handle errors here, e.g., display an error message to the user
-            loadingSpan.classList.add('hidden'); // Hide the loading indicator in case of an error
-        });
-});
+            .then(response => response.json())
+            .then(responseText => {
+                const hasTravelKeywords = containsTravelKeywords(responseText.response);
+                if (hasTravelKeywords) {
 
-// Optionally, you can add a reset functionality
-resetButton.addEventListener("click", function () {
-    input.value = ""; // Clear the input field
-    spanGeneratedTravel.textContent = ""; // Clear the displayed result
-});
+                    spanGeneratedTravel.innerText = responseText.response.substring(0, 900);
+                } else {
+
+                    spanGeneratedTravel.innerText = "I'm sorry, I couldn't generate a travel plan description for that input. Please try again.";
+                }
+                loadingSpan.classList.add('hidden');
+            })
+            .catch(error => {
+                console.error("Error:", error);
+
+                loadingSpan.classList.add('hidden');
+            });
+    });
+
+
+    function containsTravelKeywords(response) {
+        const travelKeywords = ["beach", "sun", "city", "rainforest", "snow", "Mountain", "spa", "Safari", "Culture", "Hiking", "camping", "backpacking", "diving", "skiing", "Nature", "tropical"];
+        const lowercasedResponse = response.toLowerCase();
+
+        return travelKeywords.some(keyword => lowercasedResponse.includes(keyword.toLowerCase()));
+    }
+
+
+
+    resetButton.addEventListener("click", function () {
+        input.value = "";
+        spanGeneratedTravel.textContent = "";
+    });
